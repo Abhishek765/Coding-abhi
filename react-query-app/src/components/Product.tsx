5d7dca66c7fa8aba966bbef1cdba58ea31357a70
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-10-24 02:33:34 +0530

    react query mutation example

--- react-query-app/src/components/Product.tsx
+++ react-query-app/src/components/Product.tsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 import { fetchProduct } from "../utils/helperFunctions";
 import { TProduct, TProductParams } from "../types/products";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Product = () => {
   const params = useParams<TProductParams>();
@@ -16,6 +17,16 @@ const Product = () => {
     staleTime: 10000, // for next 10 seconds (after the first fetch) the data will be cached (no new request), after 10 seconds the data will be state then it will refetch the data again,
   });
 
+  // Mutations
+  const mutation = useMutation<{ data: TProduct }, Error, unknown>({
+    mutationFn: (newProduct) => {
+      return axios.put(
+        `https://dummyjson.com/products/${params.productId}`,
+        newProduct
+      );
+    },
+  });
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
@@ -24,7 +35,30 @@ const Product = () => {
     return <p>Error</p>;
   }
 
-  return <div>{product?.title}</div>;
+  return (
+    <>
+      <div>{product?.title}</div>
+
+      <>
+        {mutation.isPending ? (
+          <p>Updating product...</p>
+        ) : mutation.isError ? (
+          <p>Error in updating</p>
+        ) : null}
+
+        {mutation.isSuccess ? <p>{mutation.data.data.title}</p> : null}
+        <button
+          onClick={() => {
+            mutation.mutate({
+              title: `Updating Product ${product?.title} to New ${product?.title}`,
+            });
+          }}
+        >
+          Create Product
+        </button>
+      </>
+    </>
+  );
 };
 
 export default Product;
