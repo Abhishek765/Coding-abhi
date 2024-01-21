c8e05d99e8768e5d69a05e08bcee064a8f5f8789
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-01-21 23:23:39 +0530

    feat: meta data static and dynamic

--- modern-nextjs-concepts/src/app/(routing)/(dynamic-routing)/products/[productId]/page.tsx
+++ modern-nextjs-concepts/src/app/(routing)/(dynamic-routing)/products/[productId]/page.tsx
@@ -1,11 +1,21 @@
-const Product = ({
-  params,
-}: {
-  params: {
-    productId: string;
+import { Metadata } from "next";
+import React from "react";
+
+type Props = {
+  params: { productId: string };
+};
+/**
+ *
+ * @description -> a function to generate dynamic metadata
+ */
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
   };
-}) => {
-  return <div>Product {params.productId}</div>;
 };
 
-export default Product;
+const ProductDetails = ({ params }: Props) => {
+  return <div>Details about the product {params.productId}</div>;
+};
+
+export default ProductDetails;
---
+++ modern-nextjs-concepts/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "This is About page",
+};
+
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
