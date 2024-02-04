6f7737e71106a55a7e7f46eb2f5f3e051a28a83e
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-02-04 16:28:19 +0530

    feat: DB connection

--- video-uploader-app/backend/src/index.ts
+++ video-uploader-app/backend/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import { connectDB } from './db/connection';
 const app = express();
 
+dotenv.config();
 const PORT = process.env.PORT || 8000;
 
+//! DB Connection
+connectDB();
+
+//! middlewares
+
+//! Routes
 app.get('/', (req, res) => {
   res.send('<h2> Hello from Home!!!</h2>');
 });
