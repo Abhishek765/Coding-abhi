cf106b539b667e3306ef799ebf784ba1bd7d83dd
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-05-19 23:41:47 +0530

    feat: headers and cookies in route handlers

--- modern-nextjs-concepts/src/app/profile/api/route.ts
+++ modern-nextjs-concepts/src/app/profile/api/route.ts
@@ -1,8 +1,26 @@
-import { NextResponse } from "next/server";
+import { cookies, headers } from "next/headers";
+import { NextRequest } from "next/server";
 
-export async function GET() {
-  return NextResponse.json({
-    message: "Profile api data",
+export const dynamic = "force-dynamic";
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+
+  console.log(requestHeaders.get("Content-Type"));
+  console.log(requestHeaders.get("Authorization"));
+  // Using Next js headers function
+  const headerList = headers();
+  console.log(headerList.get("Content-Type"));
+  console.log(headerList.get("Authorization"));
+  // Setting the cookie
+  cookies().set("temperature", "37");
+
+  // Getting the cookies
+  const temperature = cookies().get("temperature");
+  console.log(temperature);
+
+  return new Response("Profile api data", {
+    headers: {
+      "Content-Type": "text/html",
+    },
   });
 }
- 
