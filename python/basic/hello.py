d67dc64340dc00675e5ccd9c13ff8275c6784405
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-06-12 02:00:29 +0530

    python: basic python code check + module

---
+++ python/basic/hello.py
@@ -0,0 +1,6 @@
+from print_name import printName
+print('Hello world')
+printName("Abhishek")
+user_name_1 = "Abhishek"
+user_name_2 = "Abhishek2"
+user_name_3 = "Abhishek3"
---
+++ python/basic/print_name.py
@@ -0,0 +1,2 @@
+def printName(name):
+    print('Name: %s' % name)
