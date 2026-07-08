c76f4789c40a8ec3dc24f59d5655819419a18434
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2026-07-09 00:18:52 +0530

    golang: hello world + simple values/data types

---
+++ golang/2_simple_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values -> data types
+	// int
+	fmt.Println(2)
+	// string
+	fmt.Println("hello world")
+	// bool
+	fmt.Println(false)
+	fmt.Println(true)
+	// floats
+	fmt.Println(2.3)
+	fmt.Println(7.5 / 2.0)
+}
