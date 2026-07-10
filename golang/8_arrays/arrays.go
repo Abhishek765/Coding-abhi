18d3cd3feb7bf8d919e8258ca3880a2b77edfcd7
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2026-07-10 23:33:20 +0530

    arrays in Go

---
+++ golang/8_arrays/arrays.go
@@ -0,0 +1,46 @@
+package main
+
+func main() {
+
+	// fixed size array
+	// var nums [4]int // by default values are initialized to zero
+	// // array length
+	// fmt.Println(len(nums))
+
+	// nums[0] = 1
+	// nums[1] = 2
+	// nums[2] = 3
+	// nums[3] = 4
+
+	// fmt.Println(nums[0])
+	// fmt.Println(nums)
+
+	// var vals [4]bool
+	// vals[2] = true
+	// fmt.Println(vals)
+
+	// var lang [4]string
+	// lang[0] = "golang"
+
+	// fmt.Println(lang)
+
+	// to declare and initialize an array
+	// nums := [4]int{1, 2, 3, 4}
+	// fmt.Println(nums)
+
+	//2d array
+	// nums := [2][3]int{
+	// 	{1, 2, 3},
+	// 	{4, 5, 6},
+	// }
+	// fmt.Println(nums)
+
+	/* if size is predictable, we can use the arrays
+	- Memory optimization
+	- Performance optimization
+	- constant time access to elements
+	- Fixed size arrays are value types, so they are copied when passed to functions
+	- Arrays are not flexible, so we use slices instead
+	*/
+
+}
