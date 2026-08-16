7aed92fabeae4a9c328bfacf430e0b09142aa953
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2026-08-16 23:04:39 +0530

    one way to read the file

--- golang/23_files/files.go
+++ golang/23_files/files.go
@@ -6,21 +6,41 @@ import (
 )
 
 func main() {
+	// f, err := os.Open("example.txt")
+	// if err != nil {
+	// 	// log the error
+	// 	panic(err)
+	// }
+
+	// fileInfo, err := f.Stat()
+	// if err != nil {
+	// 	// log the error
+	// 	panic(err)
+	// }
+
+	// fmt.Println("FileName: ", fileInfo.Name())
+	// fmt.Println("Is Directory: ", fileInfo.IsDir())
+	// fmt.Println("Size: ", fileInfo.Size())
+	// fmt.Println("File Permission: ", fileInfo.Mode())
+	// fmt.Println("File modified at: ", fileInfo.ModTime())
+
+	//1. Read file
 	f, err := os.Open("example.txt")
 	if err != nil {
-		// log the error
 		panic(err)
 	}
 
-	fileInfo, err := f.Stat()
+	defer f.Close() // important after opening is done for this file we can immediately close it so that os is not busy
+
+	buf := make([]byte, 12)
+
+	l, err := f.Read(buf)
 	if err != nil {
-		// log the error
 		panic(err)
 	}
 
-	fmt.Println("FileName: ", fileInfo.Name())
-	fmt.Println("Is Directory: ", fileInfo.IsDir())
-	fmt.Println("Size: ", fileInfo.Size())
-	fmt.Println("File Permission: ", fileInfo.Mode())
-	fmt.Println("File modified at: ", fileInfo.ModTime())
+	for i := 0; i < len(buf); i++ {
+		fmt.Println("data", l, string(buf[i]))
+	}
+
 }
