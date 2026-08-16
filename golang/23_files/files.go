51072ccd8f780020faa5c87047151655c56efebb
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2026-08-17 00:12:52 +0530

    delete a file

--- golang/23_files/files.go
+++ golang/23_files/files.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 )
@@ -82,37 +81,44 @@ func main() {
 
 	// read and write to another file (streaming fashion)
 
-	sourceFile, err := os.Open("example.txt")
+	// sourceFile, err := os.Open("example.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer sourceFile.Close()
+
+	// destFile, err := os.Create("example2.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer destFile.Close()
+
+	// reader := bufio.NewReader(sourceFile)
+	// writer := bufio.NewWriter(destFile)
+
+	// for {
+	// 	b, err := reader.ReadByte()
+	// 	if err != nil {
+	// 		if err.Error() != "EOF" {
+	// 			panic(err)
+	// 		}
+	// 		break
+	// 	}
+
+	// 	e := writer.WriteByte(b)
+	// 	if e != nil {
+	// 		panic(e)
+	// 	}
+	// }
+
+	// writer.Flush()
+	// fmt.Println("Written is completed")
+
+	// Delete a file
+	err := os.Remove("example2.txt")
 	if err != nil {
 		panic(err)
 	}
-	defer sourceFile.Close()
-
-	destFile, err := os.Create("example2.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer destFile.Close()
-
-	reader := bufio.NewReader(sourceFile)
-	writer := bufio.NewWriter(destFile)
-
-	for {
-		b, err := reader.ReadByte()
-		if err != nil {
-			if err.Error() != "EOF" {
-				panic(err)
-			}
-			break
-		}
-
-		e := writer.WriteByte(b)
-		if e != nil {
-			panic(e)
-		}
-	}
-
-	writer.Flush()
-	fmt.Println("Written is completed")
+	fmt.Println("File Deleted successfully")
 
 }
